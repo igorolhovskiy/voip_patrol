78b57b1709e10681dd291c04ee6e343217157d0b
Ihor Olkhovskyi <ihor.olkhovskyi@cern.ch>
2023-11-21 13:46:40 +0100

    Make voip_patrol compatible with lastest version of pjsua

--- include/config_site.h
+++ include/config_site.h
@@ -17,3 +17,5 @@
 #define PJMEDIA_SRTP_HAS_DTLS           1
 // Make send of "100 - Trying" explicit
 #define PJSUA_DISABLE_AUTO_SEND_100 1
+// Disable detection of merged requests
+# define PJSUA_DETECT_MERGED_REQUEST    0

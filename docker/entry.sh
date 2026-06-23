97b8c9b37f894469f22f962afbed80b1aa477c49
Ihor Olkhovskyi <ihor.olkhovskyi@cern.ch>
2026-06-23 17:06:38 +0200

    better docker file

--- docker/entry.sh
+++ docker/entry.sh
@@ -3,18 +3,11 @@
 RESULT_FILE=${RESULT_FILE-"result.json"}
 PORT=${PORT="5060"}
 
-if [ -z "${XML_CONF}" ]; then
-    for SCENARIO in /xml/*.xml; do
-        echo "------ Running ${SCENARIO} -------"
+echo " >--- Running scenario ${XML_CONF}"
 
-        /git/voip_patrol/voip_patrol --port ${PORT} --conf ${SCENARIO} --output /output/${RESULT_FILE}
+/git/voip_patrol/voip_patrol --port ${PORT} --conf /xml/${XML_CONF}.xml --output /output/${RESULT_FILE} --log-level-file ${LOG_LEVEL_FILE} --log-level-console ${LOG_LEVEL}
 
-        echo "---- Done running ${SCENARIO} ----"
-    done
-else
-    echo "Running 1 scenario: ${XML_CONF}"
-    /git/voip_patrol/voip_patrol --port ${PORT} --conf /xml/${XML_CONF} --output /output/${RESULT_FILE}
-fi
+echo " ---> Scenario ${XML_CONF} done"
 
 chmod 777 /output
 chmod 666 /output/${RESULT_FILE}
